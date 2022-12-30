70e69c2f99b52aba911f32f40d970d2dacdb596e
Gregtmj <greg_1461996@hotmail.com>
2022-12-30 13:22:38 +0300

    tests for local

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,9 +1,35 @@
-from page_analyzer import app
+import re
 
 
-def test_homepage():
-    response = app.test_client().get('/')
+def test_homepage(client):
+    """
+    :param client:
+    :return:
+    """
+    response = client.get('/')
+    print(response.json)
 
     assert response.status_code == 200
-    assert b"<title> Homepage </title>" in response.data
     assert b"Hello from Greg!" in response.data
+
+
+def test_urls(client):
+    response = client.get('/urls')
+
+    assert response.status_code == 200
+
+
+def test_post_url(client):
+    data = {
+        'url': 'https://web.whatsapp.com/'
+    }
+    response = client.post('/urls',
+                           data=data)
+    assert response.status_code == 301
+
+    response_data: str = response.get_data().decode('utf-8')
+    match = re.findall(r'urls/\d', response_data)[0]
+    url_id: int = int(match.split('/')[1])
+
+    response = client.get(f'/urls/{url_id}')
+    assert response.status_code == 200
