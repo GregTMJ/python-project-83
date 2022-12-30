aec45449fccf45e7a8f03628b15f32ac2b7dc048
Gregtmj <greg_1461996@hotmail.com>
2022-12-30 13:21:26 +0300

    initialization for db

---
+++ database.sql
@@ -0,0 +1,18 @@
+drop table if exists url_checks CASCADE ;
+drop table if exists urls CASCADE ;
+
+create table urls(
+id serial primary key,
+name varchar(255) unique,
+created_at DATE DEFAULT CURRENT_TIMESTAMP
+);
+
+create table url_checks(
+    id serial primary key,
+    url_id integer references urls (id) on delete cascade on update cascade,
+    status integer,
+    h1 varchar(255),
+    title varchar(255),
+    description varchar(255),
+    created_at DATE DEFAULT CURRENT_TIMESTAMP
+)
